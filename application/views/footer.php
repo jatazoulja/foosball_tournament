e426941d13d3b24ff71e98af007b50b9f5ff3140
Abastillas, Peter Eman <PeterEman.Abastillas@asurion.com>
2015-10-13 16:43:37 +0800

    reinitiate

--- application/views/footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<script src="<?=base_url()?>js/vendor/jquery-1.10.2.min.js"></script>
-<script src="<?=base_url()?>js/plugins.js"></script>
-<script src="<?=base_url()?>js/bootstrap.min.js"></script>
-
-</body>
-</html>
